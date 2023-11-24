6a4fb3b0cbd782fd5edb0fb13b49a9219d80108e
CHeessoo <gabycho41@gmail.com>
2023-11-24 11:28:16 +0900

    20231124

---
+++ finalhanbando/src/main/java/com/gdu/finalhanbando/dto/FinalhanbandoApplication.java
@@ -0,0 +1,13 @@
+package com.gdu.finalhanbando.dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalhanbandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalhanbandoApplication.class, args);
+	}
+
+}
---
+++ finalhanbando/src/test/java/com/gdu/finalhanbando/FinalhanbandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdu.finalhanbando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalhanbandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
